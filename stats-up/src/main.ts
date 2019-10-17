c7831cd5f8aca2e8709f8041d131ae69b5fde3d6
Stephen <vipers083@gmail.com>
2019-10-17 21:36:28 +0800

    Adding sidebar navigation

--- stats-up/src/main.ts
+++ stats-up/src/main.ts
@@ -3,6 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { MenuController } from '@ionic/angular';
 
 if (environment.production) {
   enableProdMode();
